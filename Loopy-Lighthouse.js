08786b5b08343c84b7ae124a25e50feb74e27d46
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-10-02 03:32:40 +0000

    Use for loop to loop through 100 to 200 with %3,%4,%3&&%4

---
+++ Loopy-Lighthouse.js
@@ -0,0 +1,19 @@
+/*
+from 100 to 200 
+If the number is a multiple of 3, print the string "Loopy" instead of the number.
+If the number is a multiple of 4, print the string "Lighthouse" instead of the number.
+If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number.
+By print, we are referring to console.log.
+*/
+for (var i=100; i<=200; i++){
+  if (i%3===0 && i%4===0){
+    console.log("LoopyLighthouse");
+  } else if (i%3 === 0){
+    console.log("Loopy");
+  } else if (i%4 === 0){
+    console.log("Lighthouse");
+  } else {
+    console.log(i);
+  }
+  
+}
