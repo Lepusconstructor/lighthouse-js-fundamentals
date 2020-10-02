6224e982d3cbea9da193412758c997c04ed5fad9
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-10-02 21:47:32 +0000

    practice function expression with isEven

---
+++ even.js
@@ -0,0 +1,8 @@
+const isEven = function (num){
+  return num % 2 === 0;
+}
+console.log(isEven(10));
+console.log(isEven(11));
+/*
+The variable tenIsEven is set to the value returned by isEven(10), namely true. elevenIsEven is set to the value returned by isEven(11), namely false.
+*/
