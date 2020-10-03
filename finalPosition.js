aeb79bb1ff20f9b58d5c1d9deb4769bb9d6de10c
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-10-03 01:39:19 +0000

    the x marks the perfect spot practice with switch statement to find out what the final position is for the parade

---
+++ finalPosition.js
@@ -0,0 +1,20 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']  
+
+const finalPosition = function (moves) {
+  let position = [0,0];
+  for (let element of moves){
+    switch(element){
+    case 'east': position[0] += 1;
+      break;
+    case 'west': position[0] -= 1;
+      break;
+    case 'north': position[1] += 1;
+      break;
+    case 'south': position[1] -= 1;
+     break;
+     
+    } 
+  }return position;
+}
+
+console.log(finalPosition(moves));
