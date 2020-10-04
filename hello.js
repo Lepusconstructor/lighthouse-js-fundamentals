152afb974250699189fa0b5f5ae20683b67fe508
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-10-04 14:01:57 +0000

    some changes

--- hello.js
+++ hello.js
@@ -3,7 +3,21 @@ let sayHello = function(){
 }
 sayHello();
 
+sayHello = () =>{ // const functionName = () =>{} zero parameter
+  var words = "Hello,hello";
+  return words;
+}
+
+
 sayHello = function(name){
   console.log("Hello"+ name);
 }
 sayHello("J");
+
+let say2para = (p1,p2)=>{// const functionName = (1stP, 2ndP) => {}
+console.log("say "+ p1+p2);
+}
+
+let sayWow = words =>{ // const functionName = 1parameter => {}
+  console.log("Wow, "+ words);
+}
