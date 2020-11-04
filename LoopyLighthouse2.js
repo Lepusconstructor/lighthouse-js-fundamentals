b64336ada9b9ebfa0782be59d38f4759f48ffd05
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-11-04 16:30:44 +0000

    untracked file and updated file added

--- LoopyLighthouse2.js
+++ LoopyLighthouse2.js
@@ -5,21 +5,48 @@ words should be an array of 2 strings representing the words that will replace t
 The loopyLighthouse function should log to the console all the numbers from 15 to 90, except replacing multiples of 2 with "Batty", multiples of 5 with "Beacon", and multiples of 2 and 5 with "BattyBeacon".
 */ 
 
+/* This worked in node but not compass
 function loopyLighthouse (range, multiples, words){
-for (var i=range[0]; i < range.legth; i++){ 
-  if (i % multiples[0] === 0 && i % multiples[1] === 0){
-    array[i] = words[0]+words[1];
-  } else if (i % multiples[0] === 0){
-    array[i] = words[0];
-  } else if (i % multiples[1] === 0){
-    array[i] = words[1];
-  } else {
-    return array[i];
+  let result = [];
+  for (var i = range[0]; i <= range[1];i++){
+    if (i % multiples[0] === 0 && i % multiples[1] === 0){
+      result.push(words[0] + words[1]) ;
+    } else if (i % multiples[0] === 0){
+      result.push(words[0]);
+    } else if (i % multiples[1] === 0){
+      result.push(words[1]);
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
+}
+  
+
+
+console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
+//console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
+*/
+
+//The following works for compass
+function loopyLighthouse (range, multiples, words){
+  
+  for (var i = range[0]; i <= range[1];i++){
+    if (i % multiples[0] === 0 && i % multiples[1] === 0){
+     console.log(words[0] + words[1]) ;
+    } else if (i % multiples[0] === 0){
+      console.log(words[0]);
+    } else if (i % multiples[1] === 0){
+      console.log(words[1]);
+    } else {
+      console.log(i);
+    }
+  
   }
   
 }
+  
 
-}
 
-loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
 console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
+//console.log(loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]));
